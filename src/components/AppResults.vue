dda52e8246e8bdf3273221024ae1cd0a55bd2abc
AlessioAbbati <alex_117@live.it>
2023-05-22 17:39:26 +0200

    add results

--- src/components/AppResults.vue
+++ src/components/AppResults.vue
@@ -1,13 +1,26 @@
 <script>
-export default {}
+import {store} from '../store';
+export default {
+    data() {
+        return {
+            store,
+        }
+    }
+}
 
 </script>
 
 <template>
-  
+  <div class="results">
+    found {{ store.CardsList.length }}
+  </div>
 </template>
 
 <style lang="scss" scoped>
-
+  .results {
+    width: 80%;
+    height: 3rem;
+    background-color: #212529;
+  }
 </style>
 
