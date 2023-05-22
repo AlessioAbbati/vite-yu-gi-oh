4b283e7e95f0171d78d664a52f623bf33df67edd
AlessioAbbati <alex_117@live.it>
2023-05-22 15:35:05 +0200

    add header with title and logo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <script>
+import AppHeader from "./components/AppHeader.vue";
+import CardsList from './components/CardsList.vue';
+import axios from 'axios';
+import { store } from './store';
+
+
+export default {
+  data() {
+    return {
+      store,
+    }
+  },
+  components: { 
+    AppHeader,
+    CardsList,
+     
+  },
+};
+
 
 </script>
 
 <template>
-  
+  <app-header />
+  <cards-list />
 </template>
 
 <style lang="scss">
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<script>
+
+</script>
+
+<template>
+    <header>
+        <div class="logo">
+            <img src="https://3.bp.blogspot.com/-wLH_qbmRoJU/TZdYCkrQuZI/AAAAAAAAAw0/G1_uzsANMI8/s1600/yugioh+logo.png" alt="">
+        </div>
+        <h1>YU-GI-OH Api</h1>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    .logo {
+        width: 10%;
+        display: flex;
+        justify-content: center;
+        img {
+            width: 60%;
+        }
+    }
+}
+</style>
